5e7a0ae5d69620632d29da3e75d4b16097c76934
Farrell Arrafi Idris <jshzhbsj@gmail.com>
2024-09-11 12:54:33 +0700

    game 2d part 2?

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.sql.SQLOutput;
 
 public class GamePanel extends JPanel implements Runnable {
 
@@ -7,24 +8,88 @@ public class GamePanel extends JPanel implements Runnable {
     final int originalTileSize = 16; // 16x16 pixel
     final int scale = 3;
 
+    int FPS = 120;
+
     final int tileSize = originalTileSize*scale; //48x48 pixsel
     final int maxScreenCol = 16;
     final int maxScreenRow = 12;
     final int screenWidth = tileSize*maxScreenCol; //768
     final int screenHeight = tileSize*maxScreenRow; //576
 
+    KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 2;
 
     public GamePanel(){
 
         this.setPreferredSize(new Dimension(screenWidth,screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
+    }
 
+    public void startGameThread(){
+        gameThread = new Thread(this);
+        gameThread.start();
     }
 
     @Override
     public void run() {
+        double drawInterval = 1000000000/FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+        int drawCount = 0;
 
+        while(gameThread != null) {
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
+
+            if (delta >= 1) {
+                update();
+                repaint();
+                delta--;
+                drawCount++;
+            }
+            if (timer >= 1000000000) {
+                System.out.println("FPS " + drawCount);
+                drawCount = 0;
+                timer = 0;
+            }
+        }
     }
+
+    public void update(){
+        if (keyH.upPressed && playerY >= 0){
+            playerY -= playerSpeed;
+        }
+        if (keyH.downPressed && playerY <= screenHeight - 50){
+            playerY += playerSpeed;
+        }
+        if (keyH.leftPressed && playerX >= 0){
+            playerX -= playerSpeed;
+        }
+        if (keyH.rightPressed && playerX <= screenWidth - 50){
+            playerX += playerSpeed;
+        }
+    }
+
+    public void paintComponent(Graphics g){
+
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D)g;
+        g2.setColor(Color.white);
+        g2.fillRect(playerX,playerY,tileSize,tileSize);
+        g2.dispose();
+    }
+
 }
